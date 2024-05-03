dfdebc69bc7288cdddaa2bb40b3f1ad49cf1c05e
nikhilshinde3534 <154227771+nikhilshinde3534@users.noreply.github.com>
2024-05-03 18:07:07 +0530

    Add files via upload

---
+++ Nqueen.java
@@ -0,0 +1,114 @@
+import java.util.*;
+
+public class Nqueen {
+    static int totalSolutions = 0;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter No of Queens: ");
+        int N = sc.nextInt();
+        int board[][] = new int[N][N];
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                board[i][j] = 0;
+            }
+        }
+        helper(board, 0, N);
+        if (totalSolutions == 0)
+            System.out.println("No solutions found.");
+        else
+            System.out.println("Total solutions: " + totalSolutions);
+        sc.close();
+    }
+
+    public static boolean helper(int board[][], int col, int N) {
+        if (col >= N) {
+            totalSolutions++;
+            System.out.println("Solution " + totalSolutions + ":");
+            printSolution(board, N);
+            return false; // change to false to find all solutions
+        }
+        boolean res = false;
+        for (int i = 0; i < N; i++) {
+            if (safe(board, col, i, N)) {
+                board[i][col] = 1;
+                res = helper(board, col + 1, N) || res;
+                board[i][col] = 0;
+            }
+        }
+        return res;
+    }
+
+    public static boolean safe(int board[][], int col, int row, int N) {
+        for (int i = 0; i < col; i++) {
+            if (board[row][i] == 1) {
+                return false;
+            }
+        }
+
+        for (int i = row, j = col; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 1) {
+                return false;
+            }
+        }
+
+        for (int i = row, j = col; i < N && j >= 0; i++, j--) {
+            if (board[i][j] == 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void printSolution(int[][] board, int N) {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (board[i][j] == 1) {
+                    System.out.print(" Q ");
+                } else {
+                    System.out.print(" _ ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+}
+
+//OUTPUT:
+
+//Enter No of Queens: 6
+//Solution 1:
+// _  _  _  Q  _  _
+// Q  _  _  _  _  _
+// _  _  _  _  Q  _
+// _  Q  _  _  _  _
+// _  _  _  _  _  Q
+// _  _  Q  _  _  _
+
+//Solution 2:
+// _  _  _  _  Q  _
+// _  _  Q  _  _  _
+// Q  _  _  _  _  _
+// _  _  _  _  _  Q
+// _  _  _  Q  _  _
+// _  Q  _  _  _  _
+
+//Solution 3:
+// _  Q  _  _  _  _
+// _  _  _  Q  _  _
+// _  _  _  _  _  Q
+// Q  _  _  _  _  _
+// _  _  Q  _  _  _
+// _  _  _  _  Q  _
+
+//Solution 4:
+// _  _  Q  _  _  _
+// _  _  _  _  _  Q
+// _  Q  _  _  _  _
+// _  _  _  _  Q  _
+// Q  _  _  _  _  _
+// _  _  _  Q  _  _
+
+//Total solutions: 4
---
+++ rsa.java
@@ -0,0 +1,87 @@
+import java.util.Scanner;
+
+public class rsa{
+
+    public static int gcd(int a, int b) {
+        if (b == 0)
+            return a;
+        return gcd(b, a % b);
+    }
+
+    public static int modInverse(int a, int m) {
+        for (int x = 1; x < m; x++)
+            if ((a * x) % m == 1)
+                return x;
+        return 1;
+    }
+
+    public static int encrypt(int message, int e, int n) {
+        return (int) (Math.pow(message, e) % n);
+    }
+
+    public static int decrypt(int ct, int d, int n) {
+        return (int) (Math.pow(ct, d) % n);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the message to be encrypted: ");
+        String message = scanner.nextLine();
+
+        System.out.print("Enter the first prime number (p): ");
+        int p = scanner.nextInt();
+
+        System.out.print("Enter the second prime number (q): ");
+        int q = scanner.nextInt();
+
+        int n = p * q;
+        int m = (p - 1) * (q - 1);
+
+        int e = 0;
+        for (int i = 2; i < m; i++) {
+            if (gcd(i, m) == 1) {
+                e = i;
+                break;
+            }
+        }
+
+        int d = modInverse(e, m);
+
+        System.out.println("Original Message is: " + message);
+        int[] encrypted = new int[message.length()];
+        System.out.println("n (modulus): " + n);
+        System.out.println("m (Euler's totient function): " + m);
+        System.out.println("e (public exponent): " + e);
+        System.out.println("d (private exponent): " + d);
+        System.out.print("Encrypted Message is: ");
+        for (int i = 0; i < message.length(); i++) {
+            int charValue = (int) message.charAt(i);
+            encrypted[i] = encrypt(charValue, e, n);
+            System.out.print(encrypted[i] + " ");
+        }
+        System.out.println();
+
+        System.out.print("Decrypted Message is: ");
+        for (int i = 0; i < encrypted.length; i++) {
+            int decrypted = decrypt(encrypted[i], d, n);
+            System.out.print((char) decrypted);
+        }
+        System.out.println();
+
+        scanner.close();
+    }
+}
+
+
+
+//Enter the message to be encrypted: helloram
+//Enter the first prime number (p): 2
+//Enter the second prime number (q): 3
+//Original Message is: helloram
+//n (modulus): 6
+//m (Euler's totient function): 2
+//e (public exponent): 0
+//d (private exponent): 1
+//Encrypted Message is: 1 1 1 1 1 1 1 1 
+//Decrypted Message is: ☺☺☺☺☺☺☺☺
